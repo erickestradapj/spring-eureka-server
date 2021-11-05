61dfed0157cb79440cea2dae330684ad0b0234a1
Erick Estrada <erickestradapj@gmail.com>
2021-11-05 08:48:03 -0500

    starting project

---
+++ src/test/java/com/dev/spring/app/eureka/ServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spring.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
